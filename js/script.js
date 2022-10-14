6726b9615bd5855a9fd2e3c908ab5451fafb439f
coder-ms <marco.sbrascia@gmail.com>
2022-10-14 17:34:10 +0300

    base es1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+let testo = document.querySelector('h1');
+let btn = document.getElementById('btnUno');
+
+
+
+function palidroma(word, arrayWord, arrayReverseWord, reverseWord){
+
+
+}
+btn.addEventListener('click', palidroma);
+
