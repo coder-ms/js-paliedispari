e81a95e45bb2b4b62fb1b904e79bab9577d8a86d
coder-ms <marco.sbrascia@gmail.com>
2022-10-14 20:15:57 +0300

    es2

--- js/script2.js
+++ js/script2.js
@@ -5,3 +5,48 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto
  */
+
+
+let evenOrOdd = document.getElementById('numberOddEven');
+let numberInput = document.getElementById('numberInput');
+let btn = document.querySelector('button');
+let textResult = document.querySelector('p');
+
+/*Funzione random number*/
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+/*Somma e verifica */
+function sumRandomNumbers()
+{
+    let numberUser = numberInput.value;
+    let numberOddEven = evenOrOdd.value;
+    //
+    console.log( "numberUser = " + numberUser);
+    console.log( "numberOddEven = " + numberOddEven);
+
+    // sommo il numero inserito con il numero random del computer
+    let numberRandom = randomNumber(1,5);
+    console.log( "numberRandom = " + numberRandom);
+
+    let somma = numberUser + numberRandom;
+    console.log( "sommauser_random = " + somma );
+    
+
+    // Stabilisco se la somma dei due numeri è pari o dispari e verifico chi ha vinto tramite ciclo if
+    if((somma % 2) === 0 && numberOddEven === 'even' ){
+        textResult.innerHTML = "You Win! Try Again";
+        //console.log('true');
+        return true;
+       
+    } 
+    else if ((somma % 2) === 0 && numberOddEven === 'odd' ) {
+        //console.log('false');
+        textResult.innerHTML = "You Lost! Try Again";
+        return false;
+    }
+}
+
+//
+btn.addEventListener('click', sumRandomNumbers);
